a55d9e1ea8e482bc69d9fa9e61cb0c58f1cc4373
HossainRobin <hellorobinhossain@gmail.com>
2023-08-19 11:33:27 +0600

    flutter assignment three

---
+++ main.dart
@@ -0,0 +1,57 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+  double milesDriven;
+
+  static int numberOfCars = 0;
+
+  Car(this.brand, this.model, this.year, this.milesDriven) {
+    numberOfCars++;
+  }
+
+  void drive(double miles) {
+    milesDriven += miles;
+  }
+
+  double getMilesDriven() {
+    return milesDriven;
+  }
+
+  String getBrand() {
+    return brand;
+  }
+
+  String getModel() {
+    return model;
+  }
+
+  int getYear() {
+    return year;
+  }
+
+  int getAge() {
+    final currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
+
+void main() {
+  Car car1 = Car('Toyota', 'Camry', 2020, 15000.0);
+  Car car2 = Car('Honda', 'Civic', 2018, 20000.0);
+  Car car3 = Car('Ford', 'Mustang', 2015, 10000.0);
+
+  car1.drive(500.0);
+  car2.drive(6100.0);
+  car3.drive(750.0);
+
+  print(
+      "Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven()} Age: ${car1.getAge()}");
+
+  print(
+      "Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven()} Age: ${car2.getAge()}");
+  print(
+      "Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven()} Age: ${car3.getAge()}");
+
+  print('Total number of cars created: ${Car.numberOfCars}');
+}
